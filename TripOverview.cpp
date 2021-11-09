718a02376a836e03ef85539ce033261ce03d8a1d
chase <>
2021-11-08 20:03:59 -0800

    Added Trip Overview class

---
+++ TripOverview.cpp
@@ -0,0 +1,8 @@
+#include "TripOverview.h"
+
+
+TripOverview::TripOverview(Trip userTrip, std::vector< Trip > data): userTrip(userTrip), dataArray(data){}
+
+TravelTime TripOverview::calcCheapestTime(){
+  
+}
---
+++ TripOverview.h
@@ -0,0 +1,20 @@
+#ifndef TRIP_OVERVIEW_H
+#define TRIP_OVERVIEW_H
+
+#include <vector>
+#include "Trip.cpp"
+#include "TravelTime.cpp"
+
+class TripOverview{
+private:
+  Trip userTrip;
+  std::vector< Trip > dataArray;
+
+public:
+  TripOverview(Trip userTrip, std::vector< Trip > data);
+
+  TravelTime calcCheapestTime();
+
+};
+
+#endif
