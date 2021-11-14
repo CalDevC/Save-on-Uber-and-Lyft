8eb7b626d41c62bf03dc5754207e675f2581fb5c
gurungsagar <gurungsagar6@gmail.com>
2021-11-14 14:32:32 -0800

    added user inputs

--- main.cpp
+++ main.cpp
@@ -6,6 +6,8 @@
 #include "FilterData.h"
 
 int main() {
+   std::string sourceLoc, destinationLoc, weather;
+   double lowerRange, higherRange;
    Trip a = Trip("424553bb-7174-41ea-aeb4-fe06d4f4b9d7", "Haymarket Square", "North Station", "Lyft", "1544952607890", 5.0, false);
    Trip b = Trip("424553bb-7174-41ea-aeb4-fe06d4f4b9d7", "Haymarket Square", "North Station", "Lyft", "1543805278271", 15.0, false);
    Trip c = Trip("424553bb-7174-41ea-aeb4-fe06d4f4b9d7", "Haymarket Square", "North Station", "Lyft", "1545098110455", 10.0, false);
@@ -26,10 +28,37 @@ int main() {
    overview.calcCheapestTravelTime(data);
 
    std::cout<<"Start Program Execution"<<"\n";
-
+   std::cout<<"Enter your location: ";
+   getline(std::cin, sourceLoc);
+   std::cout<<"Enter your destination: ";
+   getline(std::cin, destinationLoc);
+   std::cout<<"Choose weather option from below:\na. no rain\nb. light rain\nc. moderate rain\nd. heavy rainning\n";
+   getline(std::cin, weather);
+   std::string tempWeather = weather;
+   if(tempWeather == "no rain"){
+      lowerRange = 0.000;
+      higherRange = 0.000;
+   } else if(tempWeather == "light rain"){
+      lowerRange = 0;
+      higherRange = 0.1000;
+   } else if(tempWeather == "moderate rain"){
+      lowerRange = 0.1000;
+      higherRange = 0.3000;
+   }
+   else{
+      lowerRange = 0.3000;
+      higherRange = 1.000;
+   }
+   
+   
+   
    std::vector<Trip> TripData = read_file_into_trip("cab_rides.csv");
    std::vector<Weather> WeatherData = read_file_into_weather("weather.csv");
 
+   std::vector<Trip> userTrip = FilterData::FilterTrips(sourceLoc, destinationLoc, TripData);
+
+   std::vector<Weather> userWeather = FilterData::FilterWeatherData(sourceLoc, destinationLoc, lowerRange, higherRange, WeatherData);
+   
    /*for(int i=0;i<TripData.size();i++)
    {
       std::cout<<TripData[i].getCompany()<<" "<<TripData[i].getDestination()<<" "<<TripData[i].getIsRaining()<<" "<<TripData[i].getPrice()<<" "<<TripData[i].getSourceLoc()<<" "<<TripData[i].getTimeStamp()<<" "<<TripData[i].getTripId()<<"\n";
